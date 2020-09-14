2525e1719ff681bdc1920996a3fabaaa2ef3893c
Tony Wang <yangwang@wisers.com>
2020-09-14 14:32:17 +0800

    [mod] take off PageView Saga

--- saga.js
+++ saga.js
@@ -1,12 +1,11 @@
 import { all, fork } from 'redux-saga/effects'
 import es6promise from 'es6-promise'
 import appSaga from 'containers/App/saga'
-import pageViewSaga from 'containers/PageView/saga'
 
 es6promise.polyfill()
 
 function* rootSaga() {
-  yield all([fork(appSaga), fork(pageViewSaga)])
+  yield all([fork(appSaga)])
 }
 
 export default rootSaga
