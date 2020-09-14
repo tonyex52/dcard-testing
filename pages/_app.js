65109b14aff387700bbaf6b0766d0796dd13b834
Tony Wang <yangwang@wisers.com>
2020-09-14 14:30:48 +0800

    [mod] remove inject app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import { ThemeProvider } from 'styled-components'
-import InjectApp from 'containers/App'
 import GlobalStyles from 'containers/App/GlobalStyles'
 import * as theme from 'constants/theme'
 import { wrapper } from '../store'
@@ -8,7 +7,6 @@ function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <InjectApp />
       <Component {...pageProps} />
     </ThemeProvider>
   )
